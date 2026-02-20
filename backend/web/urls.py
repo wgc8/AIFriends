cc75b75cfdae665bcff0345eca5bf30805901e3a
wgc <wgc8cgw@163.com>
2026-02-20 14:32:37 +0800

    完成后端api

--- backend/web/urls.py
+++ backend/web/urls.py
@@ -1,16 +1,18 @@
 from django.urls import path
 from django.conf import settings
 from django.views.static import serve  # 处理静态/媒体文件的核心视图
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
 
 from web.views.index import index
+from web.views.user.account.login import LoginView
+from web.views.user.account.logout import LogoutView
+from web.views.user.account.refresh_token import RefreshTokenView
+from web.views.user.account.register import RegisterView
 
 urlpatterns = [
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/user/account/login/', LoginView.as_view()),
+    path('api/user/account/logout/', LogoutView.as_view()),
+    path('api/user/account/register/', RegisterView.as_view()),
+    path('api/user/account/refresh_token/', RefreshTokenView.as_view()),
     #用serve视图处理media文件请求
     path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT}),
     # SPA入口：所有非media/非api的请求指向index
