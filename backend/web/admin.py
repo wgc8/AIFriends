9389133163a60d95c632020e42bd08ad37dc7434
wgc <wgc8cgw@163.com>
2026-02-22 14:00:05 +0800

    1,修复logout退出bug;2,重置数据库；3，创建character数据库；4，实现charactor的增删改查接口

--- backend/web/admin.py
+++ backend/web/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from web.models.user import UserProfile
+from web.models.character import Character
 # Register your models here.
 
 @admin.register(UserProfile)
@@ -8,3 +9,10 @@ class UserProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'profile', 'create_time', 'update_time')
     search_fields = ('user__username', 'profile')
     list_filter = ('create_time', 'update_time')
+
+@admin.register(Character)
+class CharacterAdmin(admin.ModelAdmin):
+    raw_id_fields = ('author',)
+    # list_display = ('author', 'name', 'profile', 'create_time', 'update_time')
+    # search_fields = ('author__user__username', 'name', 'profile')
+    # list_filter = ('create_time', 'update_time')
