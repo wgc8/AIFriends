b1867c99ad8dd8f48a9c8af7ace9474705dde40b
wgc <wgc8cgw@163.com>
2026-02-25 00:51:10 +0800

    feat: add character list retrieval and user profile display functionality

--- backend/web/urls.py
+++ backend/web/urls.py
@@ -13,6 +13,7 @@ from web.views.create.character.create import CreateCharacterView
 from web.views.create.character.update import UpdateCharacterView
 from web.views.create.character.remove import RemoveCharacterView
 from web.views.create.character.get_single import GetSingleCharacterView
+from web.views.create.character.get_list import GetListCharacterView
 
 urlpatterns = [
     path('api/user/account/login/', LoginView.as_view()),
@@ -26,6 +27,7 @@ urlpatterns = [
     path('api/create/character/update/', UpdateCharacterView.as_view()),
     path('api/create/character/remove/', RemoveCharacterView.as_view()),
     path('api/create/character/get_single/', GetSingleCharacterView.as_view()),
+    path('api/create/character/get_list/', GetListCharacterView.as_view()),
     # SPA入口：所有非media/非api的请求指向index
     path('', index),
     # 兜底路由：所有非media/非static/非assets的请求指向index（注意顺序，必须放在最后）
