a61996b51c0cee23364a1b6c5149a71cafa48855
wgc <wgc8cgw@163.com>
2026-02-24 14:16:45 +0800

    修改人物删除逻辑

--- backend/web/views/create/character/remove.py
+++ backend/web/views/create/character/remove.py
@@ -3,14 +3,18 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
 from web.models.character import Character
-
+from web.views.utils.photo import remove_photo
 
 class RemoveCharacterView(APIView):
     permission_classes = [IsAuthenticated]
     def post(self, request):
         try:
             character_id = request.data['character_id']
-            if 0 < Character.objects.filter(pk=character_id, author__user=request.user).delete():
+            character = Character.objects.get(pk=character_id, author__user=request.user)
+            if character:
+                remove_photo(character.photo)
+                remove_photo(character.background_image)
+                character.delete()
                 return Response({
                     'result': 'success',
                 })
