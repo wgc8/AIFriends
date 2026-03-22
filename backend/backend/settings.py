710e05b7ceb5bbf0fadb995d1539dd6d2ae5f886
wgc8 <wgc8cgw@163.com>
2026-03-22 23:29:11 +0800

    补充上传

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 from dotenv import load_dotenv
 load_dotenv()
+import mimetypes
+
+mimetypes.add_type("application/javascript", ".mjs", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
