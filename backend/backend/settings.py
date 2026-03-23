3494adb059890f5f24eefab77dd2151981d6a38f
wgc8 <wgc8cgw@163.com>
2026-03-23 22:27:52 +0800

    解决django 不识别*.mjs 的type=javascript

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -16,6 +16,8 @@ load_dotenv()
 import mimetypes
 
 mimetypes.add_type("application/javascript", ".mjs", True)
+mimetypes.add_type("application/wasm", ".wasm", True)
+mimetypes.add_type("model/onnx", ".onnx", True)  # 可选但建议
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
